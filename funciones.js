91642386fa81eb1bc0987a660a4cf30d68781752
ZahidN09 <zahid09nazateej@gmail.com>
2026-09-15 18:13:33 -0500

    Calcular saldo diponible

--- funciones.js
+++ funciones.js
@@ -1,1 +1,9 @@
 //AQUI TODA LA LOGICA DE LAS FUNCIONES DEL NEGOCIO
+
+function calcularDisponible(ingresos,egresos){
+    if(ingresos > egresos){
+        return ingresos-egresos;
+    }else{
+        return 0;
+    }
+}
--- simulador.js
+++ simulador.js
@@ -1,1 +1,14 @@
 //AQUI EL JAVASCRIPT PARA MANIPULAR EL HTML
+
+function calcular(){
+    let cmpIngresos = document.getElementById("txtIngresos");
+    let ingresos = parseFloat(cmpIngresos.value);
+
+    let cmpEgresos = document.getElementById("txtEgresos");
+    let egresos = parseFloat(cmpEgresos.value);
+
+    let saldo = calcularDisponible(ingresos,egresos);
+
+    let lblDisponibleValor = document.getElementById("spnDisponible");
+    lblDisponibleValor.textContent = saldo.toFixed(2);
+}
