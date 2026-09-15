54e579b2ca7ede5f8e5258d38eabc6e41a50d3ef
ZahidN09 <zahid09nazateej@gmail.com>
2026-09-15 18:38:35 -0500

    Optimizacion de código con utilitarios.js

--- funciones.js
+++ funciones.js
@@ -11,3 +11,7 @@ function calcularDisponible(ingresos, egresos) {
 function calcularCapacidadPago(montoDisponible) {
     return montoDisponible * 0.5;
 }
+
+function calcularInteresSimple(monto,tasa,plazoAnios){
+    return plazoAnios*monto*tasa/100;
+}
--- simulador.js
+++ simulador.js
@@ -1,18 +1,13 @@
 //AQUI EL JAVASCRIPT PARA MANIPULAR EL HTML
 
 function calcular(){
-    let cmpIngresos = document.getElementById("txtIngresos");
-    let ingresos = parseFloat(cmpIngresos.value);
-
-    let cmpEgresos = document.getElementById("txtEgresos");
-    let egresos = parseFloat(cmpEgresos.value);
-
+    //Calculo de valor disponible
+    let ingresos = recuperarFloat("txtIngresos");
+    let egresos = recuperarFloat("txtEgresos");
     let saldo = calcularDisponible(ingresos,egresos);
-
-    let lblDisponibleValor = document.getElementById("spnDisponible");
-    lblDisponibleValor.textContent = saldo.toFixed(2);
-
+    mostarEnSpan("spnDisponible",saldo);
+ 
+    //Calculo de capacidad de pago
     let capacidadDePago = calcularCapacidadPago(saldo);
-    let lblCapacidadValor = document.getElementById("spnCapacidadPago");
-    lblCapacidadValor.textContent = capacidadDePago.toFixed(2);
+    mostarEnSpan("spnCapacidadPago",capacidadDePago);
 }
